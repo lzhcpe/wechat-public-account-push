5e84e710043ba8c75918ff56854b490690a148b7
lzhcpe <111734770+lzhcpe@users.noreply.github.com>
2022-10-01 01:09:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o930j50Ih-Rv7Kv7Id1SmF1hAQq0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'D4SSrz5WBA5hP5Xk0chDyaa0H5s78SAzSFfQqegPfKs',
+      useTemplateId: 'DttvyaQjs8yFJvlD_9VI4cZygzw_6TdBfHmtlEAVrvo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-24',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'D4SSrz5WBA5hP5Xk0chDyaa0H5s78SAzSFfQqegPfKs',
+  CALLBACK_TEMPLATE_ID: 'DttvyaQjs8yFJvlD_9VI4cZygzw_6TdBfHmtlEAVrvo',
 
   CALLBACK_USERS: [
     {
